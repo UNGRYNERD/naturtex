c8a2deb839fe0fc0f34e797caea6162fbde509df
Daniel Aguilar <dacmail@gmail.com>
2019-01-29 12:47:53 +0100

    Instagram block

--- wp-content/themes/naturtex/resources/views/partials/footer.blade.php
+++ wp-content/themes/naturtex/resources/views/partials/footer.blade.php
@@ -1,5 +1,9 @@
 <section class="instagram">
   <div class="container container--full">
+    <h2 class="instagram__title">
+      <img src="@asset('images/instagram.png')" alt="Instagram">
+      {{ esc_html__('@naturtex_sl', 'naturtex') }}
+    </h2>
     {!! do_shortcode('[instagram-feed showbutton=false showfollow=false user="naturtex_sl"]') !!}
   </div>
 </section>
