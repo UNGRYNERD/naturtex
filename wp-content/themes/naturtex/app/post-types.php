934684d0afdf47377b3b8d6a7dbdf7722749609f
Daniel Aguilar <dacmail@gmail.com>
2019-01-30 17:32:46 +0100

    Nuevo post type Rugs

--- wp-content/themes/naturtex/app/post-types.php
+++ wp-content/themes/naturtex/app/post-types.php
@@ -88,10 +88,81 @@ add_action('init', function () {
         'show_in_nav_menus'          => true,
         'show_tagcloud'              => false,
         'rewrite'                    => array(
-            'slug'                       => 'project-type',
+            'slug'                       => 'fabrics-material',
             'with_front'                 => true,
             'hierarchical'               => false,
         )
     );
     register_taxonomy('un_fabric_material', array('un_fabric'), $args);
+});
+
+add_action('init', function () {
+    $args = array(
+        'label'                 => __('Rugs', 'naturtex'),
+        'supports'              => array('title', 'editor', 'thumbnail'),
+        'taxonomies'            => array('un_rug_material', 'un_rug_type'),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-heart',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'rewrite'               => array(
+            'slug'                  => 'rugs',
+            'with_front'            => true,
+            'pages'                 => true,
+            'feeds'                 => false,
+        ),
+        'capability_type'       => 'page',
+    );
+    register_post_type('un_rug', $args);
+}, 0);
+
+
+add_action('init', function () {
+    $args = array(
+        'hierarchical'               => true,
+        'public'                     => true,
+        'labels'                     => array(
+            'name'                   => __('Materials', 'naturtex'),
+            'singular_name'          => __('Material', 'naturtex')
+        ),
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => false,
+        'rewrite'                    => array(
+            'slug'                       => 'rugs-material',
+            'with_front'                 => true,
+            'hierarchical'               => false,
+        )
+    );
+    register_taxonomy('un_rug_material', array('un_rug'), $args);
+});
+
+add_action('init', function () {
+    $args = array(
+        'hierarchical'               => true,
+        'public'                     => true,
+        'labels'                     => array(
+            'name'                   => __('Types', 'naturtex'),
+            'singular_name'          => __('Type', 'naturtex')
+        ),
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => false,
+        'rewrite'                    => array(
+            'slug'                       => 'rugs-type',
+            'with_front'                 => true,
+            'hierarchical'               => false,
+        )
+    );
+    register_taxonomy('un_rug_type', array('un_rug'), $args);
 });
